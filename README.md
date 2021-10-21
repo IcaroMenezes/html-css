c9f36a520d44281c0afe70ca46b83340014c796c
Icaro Menezes <92949217+IcaroMenezes@users.noreply.github.com>
2021-10-21 16:01:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# html-css
+ Meus materiais de estudos em HTML 5 e CSS3 do curso em video
---
+++ testes/barra lateral/scripts.js
@@ -0,0 +1,9 @@
+function abrirNav(){
+    document.getElementById("oculto").style.width="250px"
+    document.getElementById("menu").style.display="none"
+}
+function fecharNav(){
+document.getElementById("oculto").style.width="0"
+document.getElementById("menu").style.marginLeft="0px"
+document.getElementById("menu").style.display="unset"
+}
